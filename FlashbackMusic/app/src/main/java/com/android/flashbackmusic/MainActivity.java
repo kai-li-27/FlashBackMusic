86f6bf67ba3b04a6ed99d82647d5b44b3cc0c73e
wongkatew <kww006@ucsd.edu>
2018-02-04 22:33:24 -0800

    songs display in a listview but buttons need more work aesthetically

--- FlashbackMusic/app/src/main/java/com/android/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/com/android/flashbackmusic/MainActivity.java
@@ -1,7 +1,18 @@
 package com.android.flashbackmusic;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +23,15 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaPlayer mediaPlayer;
+    // private MediaPlayer mediaPlayer;
     private static final int MEDIA_RES_ID = R.raw.jazz_in_paris;
     private ArrayList<Song> songsList;
     private ListView songsView;
+    private SongsService songsServ;
+    private Intent playIntent;
+    private boolean isMusicBound = false;
 
+    /*
     public void loadMedia(int resourceId) {
         if (mediaPlayer == null) {
             mediaPlayer = new MediaPlayer();
@@ -36,25 +51,59 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             System.out.println(e.toString());
         }
+    } */
+
+    public void chosenSong(View view) {
+        songsServ.setSong(Integer.parseInt(view.getTag().toString()));
+        songsServ.playSong();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (playIntent == null) {
+            playIntent = new Intent(this, SongsService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        /*
         if (isChangingConfigurations() && mediaPlayer.isPlaying()) {
             ; //"do nothing"
         }
+        */
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mediaPlayer.release();
+        songsServ = null;
+        stopService(playIntent);
+        // mediaPlayer.release();
     }
 
     public void getSongsList() {
-        // TODO, FIXME
-        songsList.add(new Song(1, "Jazz in Paris", "Human", "Jazzy"));
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if (musicCursor != null && musicCursor.moveToFirst()) {
+            int titleColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.ARTIST);
+            // TODO grabbing album title
+            do {
+                long currId = musicCursor.getLong(idColumn);
+                String currTitle = musicCursor.getString(titleColumn);
+                String currArtist = musicCursor.getString(artistColumn);
+                // TODO same for album title
+                songsList.add(new Song(currId, currTitle, currArtist, "beep")); // FIXME album input
+            } while (musicCursor.moveToNext());
+        }
     }
 
 
@@ -63,10 +112,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                return;
+            }
+        }
+
         songsView = (ListView) findViewById(R.id.song_list);
         songsList = new ArrayList<Song>();
         getSongsList();
-        loadMedia(MEDIA_RES_ID); // load jazz in paris REMOVE LATER
+        // loadMedia(MEDIA_RES_ID); // load jazz in paris REMOVE LATER
 
         // could sort alphabetically for the songs
 
@@ -79,12 +135,12 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener (
                 new View.OnClickListener() {
                     @Override
-                    public void onClick(View view) {
+                    public void onClick(View view) { /*
                         if (!mediaPlayer.isPlaying()) {
                             mediaPlayer.start();
                         } else {
                             mediaPlayer.pause();
-                        }
+                        }*/
 
                     }
                 });
@@ -93,10 +149,26 @@ public class MainActivity extends AppCompatActivity {
         resetButton.setOnClickListener (
                 new View.OnClickListener() {
                     @Override
-                    public void onClick(View view) {
+                    public void onClick(View view) { /*
                         mediaPlayer.reset();
-                        loadMedia(MEDIA_RES_ID);
+                        loadMedia(MEDIA_RES_ID);*/
                     }
                 });
     }
+
+    private ServiceConnection musicConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SongsService.MusicBinder binder = (SongsService.MusicBinder)service;
+            songsServ = binder.getService();
+            songsServ.setList(songsList);
+            isMusicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isMusicBound = false;
+        }
+    };
+
 }
---
+++ FlashbackMusic/app/src/main/java/com/android/flashbackmusic/SongsService.java
@@ -0,0 +1,107 @@
+package com.android.flashbackmusic;
+
+import android.app.Service;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kate on 2/4/2018. Allows the songs to actually play.
+ */
+
+public class SongsService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
+
+    private MediaPlayer player;
+    private ArrayList<Song> songs;
+    private int songIndex;
+    private final IBinder musicBind = new MusicBinder();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return musicBind;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        player.stop();
+        player.release();
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    // starts playing the music
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+
+    public void setList(ArrayList<Song> inSongs) {
+        songs = inSongs;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        songIndex = 0;
+        player = new MediaPlayer();
+
+        initializeMusicPlayer();
+    }
+
+    public void setSong(int songPos) {
+        songIndex = songPos;
+    }
+
+    public void playSong() {
+        player.reset();
+        Song songToPlay = songs.get(songIndex);
+        long currSongId = songToPlay.getId();
+
+        // FIXME
+        Uri trackUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, currSongId);
+        try {
+            player.setDataSource(getApplicationContext(), trackUri);
+        } catch (Exception e) {
+            Log.e("SONGS SERVICE", "Error setting the data source", e);
+        }
+
+        player.prepareAsync();
+    }
+
+    public void initializeMusicPlayer() {
+        // let the music keep playing if it's already playing if it sleeps
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
+    }
+
+    // for interaction between the activity and SongsService
+    public class MusicBinder extends Binder {
+        SongsService getService() {
+            return SongsService.this;
+        }
+    }
+}
