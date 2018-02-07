2fdd53d41f86f3d08677f8183b6979ac22bfba7c
camron chermak <cchermak@ucsd.edu>
2018-02-06 17:26:14 -0800

    Outline of tabs

---
+++ FlashbackMusic/app/src/main/java/com/android/flashbackmusic/IndividualSong.java
@@ -0,0 +1,13 @@
+package com.android.flashbackmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndividualSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_song);
+    }
+}
