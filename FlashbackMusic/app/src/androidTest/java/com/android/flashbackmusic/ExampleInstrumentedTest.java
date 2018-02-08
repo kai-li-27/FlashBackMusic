7118de4fa844c41d37f2668da8cd32787c61114e
ECSanchez <ecsanchez97@gmail.com>
2018-02-07 16:24:30 -0800

    finished basic database functionality

--- FlashbackMusic/app/src/androidTest/java/com/android/flashbackmusic/ExampleInstrumentedTest.java
+++ FlashbackMusic/app/src/androidTest/java/com/android/flashbackmusic/ExampleInstrumentedTest.java
@@ -1,11 +1,17 @@
 package com.android.flashbackmusic;
 
+import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +22,47 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    private SongDao songDao;
+    private SongDatabase Db;
 
-        assertEquals("com.android.flashbackmusic", appContext.getPackageName());
+    @Before
+    public void createDb() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        Db = Room.inMemoryDatabaseBuilder(context, SongDatabase.class).build();
+        songDao = Db.songDao();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        Db.close();
+    }
+
+    @Test
+    public void writeUserAndReadInList() throws Exception {
+        Song song = new Song(0,"titleName", "artistName", "AlbumName");
+        songDao.insertSong(song);
+
+        Song song1 = songDao.isIntheDB("titleName", "artistName", "AlbumName");
+        assertEquals("titleName", song1.getTitle());
+
+
+        song.setLastLocation(6);
+        System.out.println(song.getLastLocation());
+        songDao.updateSong(song);
+        Song song2 = songDao.isIntheDB("titleName", "artistName", "AlbumName");
+
+
+        System.out.println("TITLE:   "+song2.getTitle());
+        System.out.println("ARTIST:  "+song2.getArtist());
+        System.out.println("ALBUM:   "+song2.getAlbum());
+        System.out.println("LASTLOC:   "+song2.getLastLocation());
+
+        assertEquals(6, song2.getLastLocation());
+
+
+        songDao.deleteSong(song);
+        song1 = songDao.isIntheDB("as", "artistName", "AlbumName");
+        assertTrue(null == song1);
+
     }
 }
--- FlashbackMusic/app/src/main/java/com/android/flashbackmusic/MainActivity.java
+++ FlashbackMusic/app/src/main/java/com/android/flashbackmusic/MainActivity.java
@@ -112,12 +112,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},1);
                 return;
             }
         }
+        */
 
         songsView = (ListView) findViewById(R.id.song_list);
         songsList = new ArrayList<Song>();
