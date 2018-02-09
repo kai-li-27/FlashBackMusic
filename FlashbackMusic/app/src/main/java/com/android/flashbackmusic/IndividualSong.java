28c2f8773022148c5bc127f57f6f639177f29e92
root <root@Camron-PC.localdomain>
2018-02-08 19:49:33 -0800

    Changed individual song layout, updated buttons and added back button

--- FlashbackMusic/app/src/main/java/com/android/flashbackmusic/IndividualSong.java
+++ FlashbackMusic/app/src/main/java/com/android/flashbackmusic/IndividualSong.java
@@ -3,6 +3,8 @@ package com.android.flashbackmusic;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IndividualSong extends AppCompatActivity {
 
@@ -12,6 +14,15 @@ public class IndividualSong extends AppCompatActivity {
         setContentView(R.layout.activity_individual_song);
         Bundle songInfo = getIntent().getExtras();
         String songName = songInfo.getString(Intent.EXTRA_TEXT);
+
+        Button goBack = (Button) findViewById(R.id.button_back);
+
+        goBack.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
