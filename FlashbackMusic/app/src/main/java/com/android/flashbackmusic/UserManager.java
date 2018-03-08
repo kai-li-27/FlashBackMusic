8747d6dc198068cecb75ef2c789ac2b888d481aa
wongkatew <kww006@ucsd.edu>
2018-03-08 13:38:24 -0800

    #65 added findUser function, but everything needs testing and refactoring

--- FlashbackMusic/app/src/main/java/com/android/flashbackmusic/UserManager.java
+++ FlashbackMusic/app/src/main/java/com/android/flashbackmusic/UserManager.java
@@ -1,7 +1,11 @@
 package com.android.flashbackmusic;
 
 
+import android.util.Log;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by kwmag on 3/5/2018. Contains the list of all users as well as sets all the anonymous
@@ -10,45 +14,91 @@ import java.util.ArrayList;
  */
 
 public class UserManager {
-    ArrayList<IUser> allUsers;
-    ArrayList<IUser> friends;
-    ArrayList<IUser> strangers; // FIXME: change this to hashmap or something with key value pair
+    private HashMap<String, IUser> allUsers;
+    private HashMap<String, IUser> friends;
+    private HashMap<String, IUser> strangers; // FIXME: change this to hashmap or something with key value pair
 
-    public ArrayList<IUser> getAllUsers() {
+    private static final String TAG = "UserManager";
+
+    public ArrayList<IUser> getUsersList(String relationship) {
+        ArrayList<IUser> users = new ArrayList<>();
+        HashMap<String, IUser> workingUsers;
+        IUser working;
+
+        if (relationship.equalsIgnoreCase("all")) {
+            workingUsers = allUsers;
+        } else if (relationship.equalsIgnoreCase("friends")) {
+            workingUsers = friends;
+        } else if (relationship.equalsIgnoreCase("strangers")) {
+            workingUsers = strangers;
+        } else {
+            Log.e(TAG, "tried to get list of users that does not exist");
+            return null;
+        }
+
+        for (String key: workingUsers.keySet()) {
+            working = workingUsers.get(key);
+            users.add(working);
+        }
+        return users;
+    }
+
+    public HashMap<String, IUser> getAllUsers() {
         return allUsers;
     }
 
+    public HashMap<String, IUser> getFriends() {
+        return friends;
+    }
+
+    public HashMap<String, IUser> getStrangers() {
+        return strangers;
+    }
+
     // set the list of users if you already have it
-    public void setAllUsers(ArrayList<IUser> allUsers) {
+    public void setAllUsers(HashMap<String, IUser> allUsers) {
         this.allUsers = allUsers;
     }
 
     // just add one individual user given the user
     public void addOneUserToList(IUser user) {
-        allUsers.add(user);
+        allUsers.put(user.getName(), user);
     }
 
     public void addOneUserToList(String name, String email, String relationship, ArrayList<Song> songs, String userId) {
         IUser working;
         if (relationship.equalsIgnoreCase("stranger")) {
-            working = new AnonymousUser("", "", email);
-            strangers.add(working);
+            name = createOneAnonymousName();
+            working = new AnonymousUser(name, "", email);
+            strangers.put(name, working);
         } else if (relationship.equalsIgnoreCase("self")) {
             working = new SelfUser("You", userId, email);
         } else {
             working = new FriendUser(name, "", email);
-            friends.add(working);
+            friends.put(name, working);
         }
         working.setListOfPlayedSongs(songs);
-        allUsers.add(working);
+        allUsers.put(name, working);
+    }
+
+    public IUser findAUser(String name) {
+        if (allUsers.get(name) == null) {
+            Log.w(TAG, "User not in list of contacts");
+        }
+        return allUsers.get(name);
     }
 
     // iterate through the users and set their anonymous names for the session
     public void createAnonymousNames() {
-        for (IUser user: strangers) {
+        for (String key: strangers.keySet()) {
             // TODO algorithm to create anonymous names Iteration 4
-            user.setName("FIXME");
+            strangers.get(key).setName("FIXME");
         }
+    }
+
+    public String createOneAnonymousName() {
+        // TODO algorithm to create anonymous names
+        return "Bob";
     }
 
 }
