4e82cbbf8a49c615f72a802333abddae5543a6d0
wongkatew <kww006@ucsd.edu>
2018-03-14 19:06:11 -0700

    citing source

--- FlashbackMusic/app/src/test/java/android/util/Log.java
+++ FlashbackMusic/app/src/test/java/android/util/Log.java
@@ -2,21 +2,23 @@ package android.util;
 
 /**
  * Created by kwmag on 3/14/2018.
+ * As recommended by https://stackoverflow.com/questions/36787449/how-to-mock-method-e-in-log
+ * "How to Mock Method e in Log" obtained 3/14/2018
  */
 
 public class Log {
     public static int d(String tag, String msg) {
-        System.out.println("DEBUG: " + tag + ": " + msg);
+        System.out.println("Log.d: " + tag + ": " + msg);
         return 0;
     }
 
     public static int i(String tag, String msg) {
-        System.out.println("INFO: " + tag + ": " + msg);
+        System.out.println("Log.i: " + tag + ": " + msg);
         return 0;
     }
 
     public static int w(String tag, String msg) {
-        System.out.println("WARN: " + tag + ": " + msg);
+        System.out.println("Log.w: " + tag + ": " + msg);
         return 0;
     }
 
