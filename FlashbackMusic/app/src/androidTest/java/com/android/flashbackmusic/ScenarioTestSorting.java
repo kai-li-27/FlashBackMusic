2608b2bdc367c7eba539ae4d743f1bb4d3043909
Kai Li <kal182@ucsd.edu>
2018-03-16 10:40:38 -0700

    #73 Added test for sort title

--- FlashbackMusic/app/src/androidTest/java/com/android/flashbackmusic/ScenarioTestSorting.java
+++ FlashbackMusic/app/src/androidTest/java/com/android/flashbackmusic/ScenarioTestSorting.java
@@ -4,15 +4,28 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.test.annotation.UiThreadTest;
+import android.support.test.espresso.ViewAction;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.widget.Spinner;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.action.ViewActions;
+import org.hamcrest.Matchers;
+
+
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mortbay.jetty.Main;
 
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
+
+import javax.net.ssl.ExtendedSSLSession;
 
 import static org.junit.Assert.assertTrue;
 
@@ -20,7 +33,9 @@ import static org.junit.Assert.assertTrue;
  * Created by kwmag on 3/15/2018.
  */
 
-public class ScenarioTestSorting extends MainActivity implements UiThreadTest{
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ScenarioTestSorting{
     ArrayList<Song> songList;
 
 
@@ -57,55 +72,13 @@ public class ScenarioTestSorting extends MainActivity implements UiThreadTest{
 
         final Spinner sortOptions = mainActivity.getActivity().findViewById(R.id.sortingOptions);
 
+        Espresso.onView(ViewMatchers.withId(R.id.sortingOptions)).perform(ViewActions.click());
+        Espresso.onData(Matchers.allOf(Matchers.is(Matchers.instanceOf(String.class)), Matchers.is("Title"))).perform(ViewActions.click());
 
-        // Looper.prepare();
-/*
-        new Thread() {
-            public void run() {
-                try {
-                    Looper.prepare();*/
-                    mainActivity.getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            // Looper.prepare();
-                            sortOptions.setSelection(1, true);
-                            System.out.println("selected: " + sortOptions.getSelectedItem());
-                            Looper.loop();
-                        }
-                    });/*
-                    Looper.loop();
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start(); */
-        /*
-        Looper.prepare();
-        Handler h = new Handler(Looper.getMainLooper());
-        h.post(new Runnable() {
-            public void run() {
-                sortOptions.setSelection(1, true);
-            }
-        });*/
 
-        /*
-        new Handler(Looper.getMainLooper().post(new Runnable() {
-            @Override
-            public void run() {
-
-                sortOptions.setSelection(1, true);
-            }
-        })); */
-        // sortOptions.setSelection(1); // sort by title pos == 1
-
-        // SongManager.getSongManager().sortByTitle();
         for (int i = 0; i < songList.size() - 1; i++) {
             assertTrue(songList.get(i).getTitle().compareTo(songList.get(i+1).getTitle()) <= 0);
         }
     }
 
-    @Override
-    public Class<? extends Annotation> annotationType() {
-        return null;
-    }
 }
